7fbe4009f99d1d108c93b347862e5cd10bd199ba
Sameer Nazeer <sameernln@gmail.com>
2020-06-14 18:19:49 +0530

    Replaced mock repo with sql repo using entity framework

---
+++ Commander/Data/SqlCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class SqlCommanderRepo : ICommanderRepo
+    {
+        private readonly CommanderContext _commanderContext;
+
+        public SqlCommanderRepo(CommanderContext commanderContext)
+        {
+            _commanderContext = commanderContext;
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _commanderContext.Commands.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _commanderContext.Commands.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
